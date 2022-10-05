4aecde825ccfe9055bfb7b28ba1db8fca1847dcb
GSuma21 <21gsuma@gmail.com>
2022-10-05 12:58:18 -0700

    #fix

--- pages/components/Who_are_we.js
+++ pages/components/Who_are_we.js
@@ -3,7 +3,7 @@ import React from "react";
 const Who_are_we = () => {
   return (
     <>
-      <h1></h1>
+      {/* <h1></h1>
       <p></p>
 
       <h1 id="headh1">Who Are We ? </h1>
@@ -12,7 +12,7 @@ const Who_are_we = () => {
           <h1>A Professional team to solve your Business Problems </h1>
         </div>
         <div className="hright">
-            <h2>A Team of CA, CS, Lawyers, Engineers and MBA's providing One step
+            <h2>A Team of CA, CS, Lawyers, Engineers and MBAs providing One step
             Solution for legal and Compliance related issues to the Startups.</h2>
         
         </div>
@@ -23,7 +23,7 @@ const Who_are_we = () => {
         is being offered in the market. We are very much sure this wll bring in
         extensive savings and complete satisfaction to all the clients.</p>
        
-      </div>
+      </div> */}
     </>
   );
 };
