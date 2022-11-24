3972f09d539693bd3f18d4d7e5d5392358577bdf
codingloop <ishunfs@gmail.com>
2022-11-24 23:16:06 +0530

    debug

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -19,6 +19,7 @@ export default async function handler(req, res) {
     };
     const res = await sgMail.send(msg);
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ message: "Failed to send email" });
   }
   return res.status(200).json({ message: "successfull" });
