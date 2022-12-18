ad90c2eda2817d13c6796d4dc69534cbcdc44265
GSuma21 <21gsuma@gmail.com>
2022-12-18 16:47:16 +0530

    issue #changes

--- pages/home.js
+++ pages/home.js
@@ -1,24 +1,23 @@
 
-import Headsection from "./components/headsection"
-import Services from "./components/services"
-import Contactform from "./components/contact"
-import About_us from "./components/about_us"
+import Headsection from "../components/headsection"
+import Services from "../components/services"
+import Contactform from "../components/contact"
+import About_us from "../components/about_us"
 
 const home = () => {
   return (
     <>
- 
-    <br />
-    <br />
-    <Headsection/>
-    <About_us/>
-    <Services/>
-    
-   <Contactform/>
-<br></br>
-<br></br>
+
+      <br />
+      <br />
+      <Headsection />
+      <About_us />
+      <Services />
+      <Contactform />
+      <br></br>
+      <br></br>
     </>
-    
+
   )
 }
 
